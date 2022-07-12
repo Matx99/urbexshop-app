3ab4037fd2273c79513a9ff3dccf9f30b111b034
mathieu.pionier <mathieu.pionnier@xelians.fr>
2022-07-12 18:46:49 +0200

    test sync with api

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,40 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <h1>LES GRANDES CATEGORIES</h1>
+      <hr>
+      <ul>
+        <li v-for="category in categories" :key="category.id">
+          <router-link :to="'/search?category=' + category.name"> -->
+            <!-- <img :src="category.img !== null ? mediaRoot+category.img.path : 'https://hearhear.org/wp-content/uploads/2019/09/no-image-icon.png' " alt="" class="icon-category"> -->
+            <!-- <p>{{ category.name }}</p> -->
+          <!-- </router-link> -->
+        <!-- </li>
+      </ul> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import axios from "axios";
+
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+    const headers = {
+    'Content-type': 'application/json',
+    // 'Authorization': 'Bearer '+token
+    } 
+    return axios.get("https://localhost/greetings",null,{headers})
+    .then(response => {
+      console.log(response.data);
+    }).catch(e => console.log(e))
+  
+  // return await axios.patch(url+`/users/${id}`,data, {headers});
   }
 }
 </script>
